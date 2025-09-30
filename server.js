9b85b73c6e2e1f41405c513426afbf5a09df1f20
Dhruv Shah <dhruv.shah@elitewaresolutions.in>
2025-09-30 16:13:20 +0530

    added cors

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
